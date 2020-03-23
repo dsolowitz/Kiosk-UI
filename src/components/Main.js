e97888ded1d40c22aa52ce532bbd62235c9f5ad1
Charles Bikhazi <charles@geekibo.com>
2020-03-23 11:07:16 -0700

    remove

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,12 @@
 import React, {useContext } from 'react';
 import {TemplateInstanceContext} from '../contexts/TemplateInstanceContext';
 import TemplateInstances from './TemplateInstances';
+import Modal from './Modal/Modal';
+import useModal from './Modal/useModal';
 
 const Main = () => {
     const {templateInstances, isLoading,selectedInstance} = useContext(TemplateInstanceContext);
+    const {isShowing, toggle} = useModal();
     if (isLoading) return (<div>Loading...</div>);
     if (templateInstances && templateInstances.length > 0) {
     return (
@@ -17,6 +20,7 @@ const Main = () => {
           );
         })}
     {selectedInstance && <div>We have selected an instance {selectedInstance.templateInstanceId}</div>}
+    {selectedInstance && <Modal isShowing={isShowing} hide={toggle} />}
     </div>);
     } else {
         return (<div>No Templates to Display</div>);
