2bff311b251bfb62701243fffdd8299c891fb291
Joey Westcott <joey@yetanotherdeveloper.com>
2020-04-05 19:03:34 -0400

    Added a default style. Clean up the demo account style.

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,32 +1,62 @@
-import React, {useContext } from 'react';
-import {TemplateInstanceContext} from '../contexts/TemplateInstanceContext';
+import React, { useContext } from 'react';
+import { TemplateInstanceContext } from '../contexts/TemplateInstanceContext';
 import TemplateInstances from './TemplateInstances';
 import Modal from './Modal/Modal';
 import Wizard from './Wizard/Wizard';
 
 const Main = () => {
-    const {accountTemplates, isLoading, selectedInstance} = useContext(TemplateInstanceContext);
-    if (isLoading) return (<div>Loading...</div>);
-    if (accountTemplates && accountTemplates.length === 1 && accountTemplates[0].templateInstances.length === 1){
-      return (<Wizard templateInstance={accountTemplates[0].templateInstances[0]} />);
-    }
-    else if (accountTemplates && accountTemplates.length > 0) {
+  const { accountTemplates, isLoading, selectedInstance } = useContext(TemplateInstanceContext);
+  if (isLoading) return (<div>Loading...</div>);
+  if (accountTemplates && accountTemplates.length === 1 && accountTemplates[0].templateInstances.length === 1) {
+    return (<Wizard templateInstance={accountTemplates[0].templateInstances[0]} />);
+  }
+  else if (accountTemplates && accountTemplates.length > 0) {
     return (
-    <div>
-        {accountTemplates.map(at => {
-          return (
-            <div className="templateHeader" key={at.templateId}>
-              <h1>{at.template.name}</h1>
-              <TemplateInstances templateInstances={at.templateInstances} />
+      <div>
+
+
+
+        <div>
+          <div className="container">
+            <div className="row">
+              <div className="col-sm">
+
+                <h3>Select a design to get started: </h3>
+
+
+              </div>
             </div>
-          );
-        })}
-        {selectedInstance  &&  <Modal/>}
-    </div>);
-    } 
-    else {
-      return (<div>No Templates to Display</div>);
-    }
+            <div className="row">
+              <div className="col-sm">
+                &nbsp;
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-sm">
+                {accountTemplates.map(at => {
+                  return (
+                    <div className="templateHeader" key={at.templateId}>
+                      <h3>{at.template.name}</h3>
+                      <TemplateInstances templateInstances={at.templateInstances} />
+                    </div>
+                  );
+                })}
+                {selectedInstance && <Modal />}
+
+
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+
+
+      </div>);
+  }
+  else {
+    return (<div>No Templates to Display</div>);
+  }
 }
 
 export default Main;
--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -9,18 +9,25 @@ const ThemeContextProvider = (props) => {
         const fetchTheme = async () => {
             try {
 
+                // apply the default theme first. 
+                const style = document.createElement("link");
+                style.href = "../themes/default/theme.css";
+                style.rel = "stylesheet";
+                style.async = true;
+
+                document.head.appendChild(style);
+
+
                 if (props.routeData && props.routeData.accountid) {
 
                     // Based on the account id in the context we should switch out to that css file. 
                     // Swap out the random number for the account id guid.
-                    //var number = Math.floor(Math.random() * Math.floor(3));
 
                     const style = document.createElement("link");
                     style.href = "../themes/" + props.routeData.accountid + "/theme.css";
                     style.rel = "stylesheet";
                     style.async = true;
 
-                    console.log(style.href)
                     document.head.appendChild(style);
                 }
 
